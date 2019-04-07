ffe2e805b58e748a0116f8902be5f092b40b53b2
alvaro-pozo-torres <alvaro.pozo.torres@gmail.com>
2019-04-07 12:14:27 -0400

    hasta leccion 138

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,13 @@
-// Global app controller
+/*----- IMPORT OPTIONS----
+import str from './models/Search';
+
+//import {add, multiply, ID} from './views/searchView';
+
+//import {add as suma, multiply as mult, ID} from './views/searchView';
+
+import * as searchView from './views/searchView';
+
+
+console.log(`Using imported values. Default: ${str}, named: ${searchView.add(2, 3)}, multiplicacion: ${searchView.multiply(5, searchView.ID)}`);
+
+*/
