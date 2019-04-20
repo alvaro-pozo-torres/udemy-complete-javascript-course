4bdb09977149ed2d59bbfecc55d119db27ba818d
alvaro-pozo-torres <alvaro.pozo.torres@gmail.com>
2019-04-19 22:14:38 -0400

    cambios antes del Spinner de Search
    cambios antes del Spinner de Search

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -11,3 +11,49 @@ import * as searchView from './views/searchView';
 console.log(`Using imported values. Default: ${str}, named: ${searchView.add(2, 3)}, multiplicacion: ${searchView.multiply(5, searchView.ID)}`);
 
 */
+
+//food2fork APÏ key: ab7712e9e5932264ad164aad21b87448 
+//food2fork URL: https://www.food2fork.com/api/search
+
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
+
+/* global state of the app
+ * - Search Object
+ * - Current recipe object
+ * - shopping list object
+ * - liked pages
+*/
+
+const state = {};
+
+const controlSearch = async () => {
+    //1. get query from the view
+    const query = searchView.getInput();
+        
+    if (query) {
+        //2. new search object and add to state
+        state.search = new Search(query);
+
+        //3. prepar UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        //4. search for recipes
+        await state.search.getResults();
+
+        //5. render results on UI
+        console.log(state.search.result)
+
+        searchView.renderResults(state.search.result)
+    }
+};
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+
+
--- 9-forkify/starter/src/js/views/searchView.js
+++ 9-forkify/starter/src/js/views/searchView.js
@@ -1,3 +1,46 @@
-export const add = (a, b) => a + b;
-export const multiply =(a, b) => a*b;
-export const ID = 10;
+import {elements} from './base';
+
+export const getInput = () => elements.searchInput.value;
+export const clearInput = () => {
+    elements.searchInput.value = '';
+    
+};
+export const clearResults = () => {
+    elements.searchResList.innerHTML = '';
+};
+
+
+const limitRecipe = (title, limit = 17)   => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((accumulator, current) => {
+            if (accumulator + current.length <= limit) {
+                newTitle.push(current);
+            }
+            return accumulator + current.length;
+        }, 0);
+        return `${newTitle.join(' ')}`+' ...';
+    }
+    return title;
+};
+
+const renderRecipe = recipe => {
+    const markup = `
+        <li>
+            <a class="results__link" href="#${recipe.recipe_id}">
+                <figure class="results__fig">
+                    <img src="${recipe.image_url}" alt="${recipe.title}">
+                </figure>
+                <div class="results__data">
+                    <h4 class="results__name">${limitRecipe(recipe.title)}</h4>
+                    <p class="results__author">${recipe.publisher}</p>
+                </div>
+            </a>
+        </li>
+    `;
+    elements.searchResList.insertAdjacentHTML('beforeend', markup);
+};
+    
+export const renderResults = recipes => {
+    recipes.forEach(renderRecipe);
+};
